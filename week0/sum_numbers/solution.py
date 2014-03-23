b45e8c21b3b92d6af547e5dc1399290b538e371e
Snezhana Spasova <snspasova@gmail.com>
2014-03-23 21:37:13 +0200

    Update solution.py

--- week0/sum_numbers/solution.py
+++ week0/sum_numbers/solution.py
@@ -1,18 +1,18 @@
 import sys
 
 def sum_numbers(filename):
-	file = open(filename,"r")
-	content=file.read().split(" ")
-	content= list(map(lambda x: int(x), content))
-	file.close()
-	return sum(content)
+    file = open(filename,"r")
+    content=file.read().split(" ")
+    content= list(map(lambda x: int(x), content))
+    file.close()
+    return sum(content)
 
 def main():
-	if len(sys.argv)>1:
-		filename = sys.argv[1]
-		print(sum_numbers(filename))
-	else:
-		print("Not enough arguments given")
+    if len(sys.argv)>1:
+        filename = sys.argv[1]
+        print(sum_numbers(filename))
+    else:
+        print("Not enough arguments given")
 
 if __name__ == '__main__':
-	main()
+    main()
